a36a81e7334d4ecbefc23afaa944fa465bf5bed7
yahyojondev <nabijonovdeveloper@gmail.com>
2023-12-07 13:35:03 +0500

    javascript

---
+++ js/main.js
@@ -0,0 +1,294 @@
+// Begin 1
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+
+// S = a * b;
+// P = 2 * (a + b);
+
+// console.log("S = " ,S);
+// console.log("P = " ,P);
+
+// Begin 2
+
+// let d = +prompt("d ni kiriting");
+
+// const p = 3.14;
+
+// L = p * d;
+// r = d / 2;
+// S = p * r ** 2 ;
+
+// console.log("L = ", L);
+// console.log("r = ", r);
+// console.log("S = ", S);
+
+// Begin 3
+
+// let a = +prompt(" a ni kiriting");
+
+// V = a ** 3;
+// S = 6 * a ** 2;
+
+// console.log("V = ",V);
+// console.log("S = ",S);
+
+// Begin 4
+
+// let a = +prompt(" a ni kiriting");
+// let b = +prompt(" b ni kiriting");
+// let c = +prompt(" c ni kiriting");
+
+// V = a*b*c;
+// S = 2 * (a * b + b * c + a * c);
+
+// console.log("V = ",V);
+// console.log("S = ",S);
+
+// Begin 5
+
+// let a = +prompt(" a kiriting");
+// let b = +prompt(" b kiriting");
+
+// M = (a + b) / 2;
+//  console.log("M = ",M);
+
+// Begin 6
+
+// let a = +prompt(" a ni kiriting");
+// let b = +prompt(" b ni kiriting");
+
+// c = ((a ** 2) + (b **2)) ** (1/2);
+// P = a + b +c;
+//  console.log("c = ",c);
+//  console.log("P = ",P);
+
+// Begin 7
+
+// let r1 = +prompt(" r1 ni kiriting");
+// let r2 = +prompt(" r2 ni kiriting");
+
+// const p = 3.14;
+
+// S1 = p * r1; 
+// S2 = p * r2;
+// S = p * ((r1 ** 2) - (r2 ** 2)); 
+
+// console.log("S1 = ",S1);
+// console.log("S2 = ",S2);
+// console.log("S = ",S);
+
+// Begin 8
+
+// let l = +prompt("l ni kiriting");
+
+// const p = 3.14;
+
+// R = l / (2 * p);
+
+// S = p * R ** 2;
+
+// console.log("R =",R);
+// console.log("S =",S);
+
+// Begin 9
+
+// let x1 = +prompt("x1 ni kiriting");
+// let x2 = +prompt("x2 ni kiriting");
+// let y1 = +prompt("y1 ni kiriting");
+// let y2 = +prompt("y2 ni kiriting");
+
+// L = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1/2);
+
+// console.log("L = ",L);
+
+// Begin 10 
+
+// let A = 100;
+// let B = 200;
+// let C = 300;
+
+// let k = C;
+// let Q = A;
+// let H = B;
+
+// A = H;
+// B = k;
+// C = Q;
+
+// console.log("A = ",H);
+// console.log("B = ",k);
+// console.log("C = ",Q);
+
+//  Begin 11
+
+// let x = +prompt("x ni kiriting");
+
+// y = 4 * ((x - 3) ** 6) - 7 * ((x - 3) ** 3) + 2;
+
+// console.log("y = ",y);
+
+// Begin 12
+
+// let A = +prompt("A ni kiriting");
+// let n = +prompt("n ni kiriting");
+
+// M = A ** n 
+
+// console.log("A ** n =",M );
+
+// Begin 13
+
+// let Tf = +prompt("Tf ni kiriting");
+
+// Tc = (Tf - 32) * (5 / 9);
+
+// console.log("Tc =",Tc);
+
+// Begin 14 
+
+// let A = +prompt("A so'm ni kiriting");
+// let X = +prompt("X ni kiriting");
+// let B = +prompt("B so'm ni kiriting");
+// let Y = +prompt("Y ni kiriting");
+
+// sh = A / X;
+// k = B / Y; 
+
+// console.log("1kg shoklad = ",sh, "so'm")
+// console.log("1kg konfet = ",k, "so'm")
+// console.log(sh - k ,"so'm qimmat");
+
+// Begin 15 
+
+// let A = +prompt("A so'm ni kiriting");
+// let X = +prompt("X ni kiriting");
+// let B = +prompt("B so'm ni kiriting");
+// let Y = +prompt("Y ni kiriting");
+
+// o1 = A / X;
+// o2 = B / Y; 
+
+// console.log("1kg olma1 = ",o1, "so'm")
+// console.log("1kg olma2 = ",o2, "so'm")
+// console.log(o1 + o2 ,"so'm savdo qildi");
+
+// integer 1 
+
+// let abc = +prompt("abc ni kiriting");
+
+// let c = abc % 10;
+// let bc = (abc % 100);
+// let  b = (bc - c) / 10;
+
+// console.log("birliklar xonasi =",c);
+// console.log("o'nliklar xonasi =",b);
+
+// integer 2
+
+// let abc = +prompt("abc ni kiriting");
+
+// let c = abc % 10;
+// let bc = (abc % 100);
+// let  b = (bc - c) / 10;
+// let a = (abc-bc) / 100;
+
+// let M = a + b +c
+
+// console.log("M =",M);
+
+// integer 3
+
+// let abc = +prompt("abc ni kiriting");
+
+// let c = abc % 10;
+// let bc = (abc % 100);
+// let  b = (bc - c) / 10;
+// let a = (abc-bc) / 100;
+
+// let M = c * 100 + b * 10 + a;
+
+// console.log("M =",M);
+
+
+// integer 4
+
+// let abc = +prompt("abc ni kiriting");
+
+// let c = abc % 10;
+// let bc = (abc % 100);
+// let  b = (bc - c) / 10;
+// let a = (abc-bc) / 100;
+
+// let M = b * 100 + a * 10 + c;
+
+// console.log("M =",M);
+
+
+// integer 5
+
+// let abc = +prompt("abc ni kiriting");
+
+// let c = abc % 10;
+// let bc = (abc % 100);
+// let  b = (bc - c) / 10;
+// let a = (abc-bc) / 100;
+
+// let M = a * 100 + c * 10 + b;
+
+// console.log("M =",M);
+
+
+// integer 6
+
+// let abcde = +prompt("abcde ni kiriting");
+
+// let ab  = Math.floor(abcde / 1000);
+// let b = ab % 10 ;
+
+
+
+// console.log("b =",b);
+
+// integer 7
+
+// let N = +prompt("N ni kiriting");
+
+// let M = N / 60 ;
+
+// console.log("minut = ",M);
+
+
+// integer 8
+
+// let N = +prompt("N ni kiriting");
+
+// let M = N / 3600 ;
+
+// console.log("soat = ",M);
+
+
+// integer 9
+
+// let N = +prompt("N ni kiriting");
+
+// let M = N / 60 ;
+
+// console.log("minut = ",M);
+// console.log("sekund = ",N);
+
+
+// integer 10
+
+// let N = +prompt("N ni kiriting");
+
+// let M = N / 3600 ;
+
+// console.log("soat = ",M);
+// console.log("sekund = ",N);
+
+
+
+
+
+
